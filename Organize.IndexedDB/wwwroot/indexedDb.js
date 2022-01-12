ab4a7850e4fbc7b56452ffba725d37855bda0518
askanymark <mark.paskanny@gmail.com>
2022-01-12 14:25:12 +0200

    fix indexed db

--- Organize.IndexedDB/wwwroot/indexedDb.js
+++ Organize.IndexedDB/wwwroot/indexedDb.js
@@ -1,15 +1,40 @@
 organizedIndexedDb = {
     db: {},
     initAsync: async () => {
-        const open = new Promise((resolve, reject) => {
-            const request = window.indexedDB.open('organize');
+        // const openRequestPromise = new Promise((resolve, reject) => {
+        //     const request = window.indexedDB.open("organize");
+        //     request.onupgradeneeded = this.onUpgradeNeeded;
+        //
+        //     request.onsuccess = function (event) {
+        //         //setTimeout(() => resolve(event.target.result), 4000);
+        //         resolve(event.target.result);
+        //     }
+        //     request.onerror = function (event) {
+        //         reject(event);
+        //     }
+        // });
+        // //await this.timeout(5000);
+        // this.db = await openRequestPromise;
+        const request = window.indexedDB.open('organize');
+        request.onupgradeneeded = event => {
+            const db = event.target.result;
+            db.createObjectStore('User', {keyPath: 'id', autoIncrement: true});
 
-            request.onupgradeneeded = this.onUpgradeNeeded;
-            request.onsuccess = event => resolve(event.target.result);
-            request.onerror = event => reject(event);
-        });
+            const textItemStore = db.createObjectStore('TextItem', {keyPath: 'id', autoIncrement: true});
+            textItemStore.createIndex('parentId', 'parentId', {unique: false});
 
-        this.db = await open;
+            const urlItemStore = db.createObjectStore('UrlItem', {keyPath: 'id', autoIncrement: true});
+            urlItemStore.createIndex('parentId', 'parentId', {unique: false});
+
+            const parentItemStore = db.createObjectStore('ParentItem', {keyPath: 'id', autoIncrement: true});
+            parentItemStore.createIndex('parentId', 'parentId', {unique: false});
+
+            const childItemStore = db.createObjectStore('ChildItem', {keyPath: 'id', autoIncrement: true});
+            childItemStore.createIndex('parentId', 'parentId', {unique: false});
+        }
+        request.onerror = () => console.error(request.error);
+        request.onsuccess = () => this.db = request.result;
+            
     },
     onUpgradeNeeded: event => {
         const db = event.target.result;
@@ -67,7 +92,7 @@ organizedIndexedDb = {
         return await new Promise((resolve, reject) => {
             const transaction = this.db.transaction(tableName, 'readwrite');
             transaction.onerror = event => reject(event);
-            const store = transaction.createObjectStore(tableName)
+            const store = transaction.objectStore(tableName)
             entityToPut.id = id;
             const request = store.put(entityToPut);
             request.onsuccess = () => resolve();
@@ -77,7 +102,7 @@ organizedIndexedDb = {
         return await new Promise((resolve, reject) => {
             const transaction = this.db.transaction(tableName, 'readwrite');
             transaction.onerror = event => reject(event);
-            const store = transaction.createObjectStore(tableName);
+            const store = transaction.objectStore(tableName);
             const request = store.delete(id);
             request.onsuccess = () => resolve();
         });
