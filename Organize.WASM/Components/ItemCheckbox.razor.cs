bc1ec5ecc8cea5c9afd440817a4d5f9b180f896d
askanymark <mark.paskanny@gmail.com>
2022-01-12 14:38:26 +0200

    fix item checkbox

--- Organize.WASM/Components/ItemCheckbox.razor.cs
+++ Organize.WASM/Components/ItemCheckbox.razor.cs
@@ -1,13 +1,20 @@
 using Microsoft.AspNetCore.Components;
 using Organize.Shared.Entities;
+using Organize.Shared.Interfaces;
 
 namespace Organize.WASM.Components;
 
 public partial class ItemCheckbox
 {
-    [Parameter]
-    public BaseItem Item { get; set; }
-    
-    [CascadingParameter]
-    public string ColorPrefix { get; set; }
+    [Parameter] public BaseItem Item { get; set; }
+
+    [CascadingParameter] public string ColorPrefix { get; set; }
+
+    [Inject] private IUserItemManager _itemManager { get; set; }
+
+    public async Task ChangeIsDone()
+    {
+        Item.IsDone = !Item.IsDone;
+        await _itemManager.UpdateAsync(Item);
+    }
 }
